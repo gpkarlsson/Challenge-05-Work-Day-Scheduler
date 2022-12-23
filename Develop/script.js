98fa245de323bda69dbe9e2d9edd340d8d4f18e1
gordonk <gpkarlsson72@gmail.com>
2022-12-22 18:03:17 -0600

    pseudocode, styled hero div

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,13 @@
-var currentTime = day.js();
+var currentTime = dayjs();
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+
+//event listener -> onclick saveButton -> save to Local Storage
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -16,7 +21,6 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   
- // if (currentTime <  
   
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -24,3 +28,14 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+
+// 1. Get current time (specific down to hour in 24hr clock) and display in hero div
+// 2. compare current time to each hour div on scheduler
+// 3. if current time = specific hour --> add present class, remove past/future (if needed)
+// 3. if current time < (before) specific hour --> add future class, remove present/past (if needed)
+// 4. if current time > (after) specific hour --> add past class, remove present/future (if needed)
+// 5. click save button stores value of inputted text on click in local storage (persists on page refresh)
+
+
